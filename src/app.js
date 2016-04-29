0210a927656ffd94a21a7e2fb2d0db76e302dfea
Johannes Schickling <schickling.j@gmail.com>
2016-04-29 22:23:41 +0200

    Updated to new relay router version

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
-import { IndexRoute, Route } from 'react-router'
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { RelayRouter, browserHistory } from 'react-router-relay'
 import Relay from 'react-relay'
+import ReactDOM from 'react-dom'
+import {
+  Router,
+  IndexRoute,
+  Route,
+  browserHistory,
+  applyRouterMiddleware,
+} from 'react-router'
+import useRelay from 'react-router-relay'
 import TodoApp from './components/TodoApp'
 import TodoList from './components/TodoList'
 import ViewerQueries from './queries/ViewerQueries'
@@ -14,7 +20,12 @@ Relay.injectNetworkLayer(
 )
 
 ReactDOM.render(
-  <RelayRouter history={browserHistory}>
+  <Router
+    forceFetch
+    environment={Relay.Store}
+    render={applyRouterMiddleware(useRelay)}
+    history={browserHistory}
+  >
     <Route
       path='/' component={TodoApp}
       queries={ViewerQueries}>
@@ -28,6 +39,6 @@ ReactDOM.render(
         queries={ViewerQueries}
       />
     </Route>
-  </RelayRouter>,
+  </Router>,
   document.getElementById('root')
 )
