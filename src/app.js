5f5fb31a4adb06e4adca689758d9c3f290dc005d
Nilan Marktanner <nilan.marktanner@student.kit.edu>
2016-10-27 15:19:35 +0200

    add comment about x-source-graphcool

--- src/app.js
+++ src/app.js
@@ -15,6 +15,8 @@ import ViewerQueries from './queries/ViewerQueries'
 
 import './style.css'
 
+// The x-graphcool-source header is to let the server know that the example app has started.
+// (Not necessary for normal projects)
 Relay.injectNetworkLayer(
   new Relay.DefaultNetworkLayer('https://api.graph.cool/relay/v1/__PROJECT_ID__', {
     headers: {
